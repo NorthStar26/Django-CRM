c2d600fd8b15e0c6c797a6723d7df3ff6c7f66ba
B1gB4dB4ng <abdullahsamurnl@gmail.com>
2025-06-30 10:30:16 +0200

    fix: correct typo in regex pattern for URL validation with port in find_urls function

--- common/serializer.py
+++ common/serializer.py
@@ -324,7 +324,7 @@ def find_urls(string):
     # http(s)://google.com
     website_regex = "^https?://[A-Za-z0-9.-]+\.[A-Za-z]{2,63}$"
     # http(s)://google.com:8000
-    website_regex_port = "^https?://[A-Za-z0-9.-]+\.[A-Zaz]{2,63}:[0-9]{2,4}$"
+    website_regex_port = "^https?://[A-Za-z0-9.-]+\.[A-Za-z]{2,63}:[0-9]{2,4}$"
     url = re.findall(website_regex, string)
     url_port = re.findall(website_regex_port, string)
     if url and url[0] != "":
